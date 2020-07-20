4bca5ab73e235afd9d26526398173370be22246d
김지예 Ji Ye Kim <65342956+jejec0703@users.noreply.github.com>
2020-07-20 14:35:18 +0900

    Add files via upload

---
+++ gmarket.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from ecommerce.items import EcommerceItem
+
+
+class GmarketSpider(scrapy.Spider):
+    name = 'gmarket'
+    allowed_domains = ['corners.gmarket.co.kr/Bestsellers']
+    start_urls = ['http://corners.gmarket.co.kr/Bestsellers']
+
+    def parse(self, response):
+        titles = response.css('div.best-list li > a::text').getall()
+        prices = response.css('div.best-list ul li div.item_price div.s-price strong span::text').getall()
+        for num, title in enumerate(titles):
+            doc = EcommerceItem()
+            doc['title'] = title
+            doc['price'] = prices[num]
+            yield doc
